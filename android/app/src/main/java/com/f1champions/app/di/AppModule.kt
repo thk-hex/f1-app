f729906f12e13e7782919649c17f5abf803d4002
Togay Han Kaya <tkaya@mobiquityinc.com>
2025-05-22 13:23:43 +0200

    fix base url

--- android/app/src/main/java/com/f1champions/app/di/AppModule.kt
+++ android/app/src/main/java/com/f1champions/app/di/AppModule.kt
@@ -20,7 +20,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    private const val BASE_URL = "http://localhost:3000"
+    private const val BASE_URL = "http://10.0.2.2:3000"
 
     @Provides
     @Singleton
