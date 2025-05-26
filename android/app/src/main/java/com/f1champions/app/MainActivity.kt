d76e2c02e69588f32d88e89384818777d71160de
Togay Han Kaya <tkaya@mobiquityinc.com>
2025-05-26 15:38:28 +0200

    use status bar

--- android/app/src/main/java/com/f1champions/app/MainActivity.kt
+++ android/app/src/main/java/com/f1champions/app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.f1champions.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,6 +23,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             F1ChampionsTheme {
                 Surface(
